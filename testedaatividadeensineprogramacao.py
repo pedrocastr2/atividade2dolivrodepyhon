b6412afd1a6f54839f30760cc0f5584baebe0ace
pedrocastr <102086416+pedrocastr2@users.noreply.github.com>
2024-09-03 14:53:27 -0300

    Add files via upload

---
+++ testedaatividadeensineprogramacao.py
@@ -0,0 +1,7 @@
+
+nome= input('por favor entre com um nome: ')
+adjectivo = input('por favor entre com um adjetivo: ')
+verbo =input('por favor entre com um verbo: ')
+objeto =input('por favor entre com um objeto: ')
+
+print("O",nome,adjectivo,verbo,"encontrar", objeto)
